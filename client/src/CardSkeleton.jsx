1f64cb63f798191f46bfd699e532ce7171ea88b8
ElemaGp <elemagodspower@gmail.com>
2022-12-21 20:52:30 +0100

    feat: created the skeleton loading for the cards/home page

---
+++ client/src/CardSkeleton.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./cardSkeleton.css"
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+const CardSkeleton = ({cards}) => {
+  return (
+    Array(cards).fill(0).map((item, index) =>
+        <div className='cardSkeleton' key={index}>
+        <div className="leftColumn">
+            <Skeleton circle width={40} height={40} />
+        </div>
+        <div className="rightColumn">
+            <Skeleton count={6} style={{fontSize: "1.5rem", marginBottom: ".6rem"}} />
+        </div>
+
+    </div>
+        )
+    
+  )
+}
+
+export default CardSkeleton
